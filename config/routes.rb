fd1f6edca88728b135071c9ec8e3b52783ffb9c0
booshanjayachandran <booshan.jayachandran@go-yubi.com>
2024-04-10 08:39:31 +0530

    logout and remember me

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get '/signup',to: 'users#new'
   get '/login',to: 'sessions#new'
   post '/login',to: 'sessions#create'
-  delete '/logout',to: 'sessions#destroy'
+  get '/logout',to: 'sessions#destroy'
   resources :users
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
