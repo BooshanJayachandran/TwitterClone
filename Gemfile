36e768cc035a824f0339ef0057d5f9d1e7c24caf
booshanjayachandran <booshan.jayachandran@go-yubi.com>
2024-04-11 18:50:45 +0530

    authorization

--- Gemfile
+++ Gemfile
@@ -51,6 +51,12 @@ gem "bootstrap-sass"
 
 gem "bcrypt"
 
+gem 'faker'
+
+gem 'will_paginate', '~> 3.1', '>= 3.1.6'
+gem 'bootstrap-will_paginate', '1.0.0'
+
+# gem 'error_highlight'
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,8 +15,13 @@ module SessionsHelper
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
-        @current_user = user end
+        @current_user = user
+      end
     end
+  end
+
+  def current_user?(user)
+    user && user == current_user
   end
   def forget(user)
     user.forget
@@ -30,5 +35,14 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
